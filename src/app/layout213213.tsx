4ca38fb88e4546b9b2357ba4da2f9ee96d6f39e0
illyadmytrenko <illadmitrenko666@gmail.com>
2025-03-13 14:00:27 +0200

    finally ?

---
+++ src/app/layout213213.tsx
@@ -0,0 +1,13 @@
+// /* eslint-disable @next/next/no-page-custom-font */
+// "use client";
+
+// import dynamic from "next/dynamic";
+
+// const DynamicRootLayout = dynamic(() => import("./root-layout"), {
+//   ssr: false,
+// });
+
+// export default function RootLayout() {
+//   return;
+//   <DynamicRootLayout />;
+// }
