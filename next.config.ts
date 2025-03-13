bd9396320242a7f16076f691f5eee31756aa1295
illyadmytrenko <illadmitrenko666@gmail.com>
2025-03-13 12:33:47 +0200

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  ignoreBuildErrors: true,
   images: {
     unoptimized: true,
     remotePatterns: [
